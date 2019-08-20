66160a7976cf80d693d26fa4e51560828b113a82
zhoulychn <zhoulychn@qq.com>
2019-08-20 12:03:21 +0800

    commit

--- src/main/java/com/zhoulychn/common/scheduler/Example.java
+++ src/main/java/com/zhoulychn/common/scheduler/Example.java
@@ -1,19 +1,17 @@
-package com.zhoulychn.scheduler;
+package com.zhoulychn.common.scheduler;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 /**
  * Created by lewis on 2017/2/12.
  * 定时器案例
  */
 
-@Component
+//@Component
 public class Example {
 
-    @Scheduled(cron = "0/10 * *  * * ? ")
+//    @Scheduled(cron = "0/10 * *  * * ? ")
     public void test() {
 
     }
