5c64ab1694e8935bdd13bbedd455b4c446712b06
zhoulychn <zhoulychn@qq.com>
2019-09-16 19:24:24 +0800

    commit

---
+++ src/main/java/com/zhoulychn/service/impl/DemoBean.java
@@ -0,0 +1,42 @@
+package com.zhoulychn.service.impl;
+
+
+import com.zhoulychn.dao.UserDAO;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+@Lazy(value = false)
+public class DemoBean implements InitializingBean, BeanNameAware, BeanFactoryAware {
+
+    public DemoBean(){
+        System.out.println("构造方法");
+    }
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("afterPropertiesSet");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("setBeanName");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("setBeanFactory");
+    }
+}
